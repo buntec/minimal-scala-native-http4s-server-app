bfc4b24d19f137b5848d883b11270e6a5512f0f0
Christoph Bunte <christophbunte@gmail.com>
2023-10-02 19:23:14 +0200

    add simple server

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # minimal-scala-native-http4s-server-app
+
+Open nix dev shell:
+
+```shell
+nix develop
+```
+
+Inside dev shell:
+
+```/shell
+./package.sh && ./app
+```
--- app.scala
+++ app.scala
@@ -1,8 +1,34 @@
-import cats.syntax.all.*
+import cats.*
 import cats.effect.*
+import cats.syntax.all.*
+import com.comcast.ip4s.*
+import org.http4s.HttpRoutes
+import org.http4s.dsl.Http4sDsl
+import org.http4s.ember.server.EmberServerBuilder
+import epollcat.EpollApp
 
-object Main extends IOApp.Simple {
+class HttpApp[F[_]: Monad] extends Http4sDsl[F] {
 
-  def run: IO[Unit] = IO.println("hello world")
+  val routes = HttpRoutes.of[F] { case GET -> Root / "alive" =>
+    Ok("I'm alive")
+  }
+
+  val app = routes.orNotFound
+
+}
+
+object Main extends EpollApp.Simple {
+
+  val h = host"localhost"
+  val p = port"8080"
+
+  def run: IO[Unit] = EmberServerBuilder
+    .default[IO]
+    .withHost(h)
+    .withPort(p)
+    .withHttpApp(HttpApp[IO].app)
+    .build
+    .evalTap(_ => IO.println(s"server listening on http://$h:$p..."))
+    .useForever
 
 }
